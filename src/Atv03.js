dc3d79f5fbee7d582b5a5cf3cd86f0cef4c0f17c
Lincoln Dias <lincolnroberto820@gmail.com>
2025-05-20 11:48:15 -0300

    Atualizando atividade 3

--- src/Atv03.js
+++ src/Atv03.js
@@ -1,12 +1,27 @@
-import Profile from "./Profile";
-import { Link } from "react-router-dom";
+import {Link} from "react-router-dom";
+import Gallery from "./Gallery.js";
+import {Profile} from "./Gallery.js";
+import Avatar from "./Avatar.js";
+import TodoList from "./TodoList.js";
+import Card from "./Profile.js";
+import PackingList from "./PackingList.js";
+import List from "./List.js";
+import TeaSet from "./TeaSet.js";
+import TeaGathering from "./TeaGathering.js";
 
-export default function Atv03() {
-    return (
-    <>
-        <h1>Atividade 3</h1>
-        <Profile />
-        <Link to="/">Voltar</Link>
-    </>
-  );
+export default function Atividade03()
+{
+    return(
+        <div>
+            <h1>Atividade 03</h1>
+            <Profile/>
+            <Card/>
+            <TodoList/>
+            <PackingList/>
+            <List/>
+            <TeaSet/>
+            <TeaGathering/>
+            <Link to="/">Voltar</Link>
+        </div>
+        );
 }
--- src/Profile.js
+++ src/Profile.js
@@ -1,54 +1,24 @@
-export default function Profile() {
-  const name = "Ada Lovelace";
-  const avatar = "https://i.imgur.com/MK3eW3Am.jpg";
-  const theme = {
-    backgroundColor: "black",
-    color: "white",
-    padding: "10px",
-  };
+import Avatar from "./Avatar.js";
 
-  function Avatar({ person = { name: "Alan" }, size = 100 }) {
-    return <img src={avatar} alt={person.name} width={size} />;
-  }
-
-  function Card({ children }) {
-    return <div className="card">{children}</div>;
-  }
-
-  const isOnline = true;
-  const showWarning = false;
-  let status;
-  if (isOnline) {
-    status = <p>Online</p>;
-  } else {
-    status = <p>Offline</p>;
-  }
-
-  const people = [
-    { id: 1, name: "Lovelace", profession: "mathematician" },
-    { id: 2, name: "Turing", profession: "chemist" },
-  ];
-  const chemists = people.filter(p => p.profession === "chemist");
-  const listItems = chemists.map(p => (
-    <li key={p.id}>{p.name} - {p.profession}</li>
-  ));
-
-  function Recipe({ drinkers }) {
-    return <p>{drinkers * 2} cups of water</p>;
-  }
-
-  return (
-    <div style={theme}>
-      <h1>{name}'s Profile</h1>
-      <Avatar person={{ name }} size={120} />
-      <Card>
-        <p>Inside a Card!</p>
-      </Card>
-      {isOnline ? <p>✅ Ativo</p> : <p>❌ Inativo</p>}
-      {showWarning && <p>Aviso!</p>}
-      {status}
-      <ul>{listItems}</ul>
-      <Recipe drinkers={3} />
+function Card({children})
+{
+   return(
+    <div className="card">
+        {children}
     </div>
-  );
+   );
+}
+
+export default function Profile(){
+    return(
+        <Card>
+            <Avatar 
+                person={{
+                    name: 'Katsuko Saruhashi',
+                    imageId: 'yfeOqp2'
+                }}
+                size={100}
+            />
+        </Card>
+    );
 }
