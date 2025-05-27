3689086ad3a63e14e6fd01fbeb4ffa14ce40ed8a
Lincoln Dias <lincolnroberto820@gmail.com>
2025-05-27 10:15:36 -0300

    corrigindo home

--- src/Home.js
+++ src/Home.js
@@ -16,6 +16,9 @@ const Home = () =>{
                         <li>
                             <Link to="Atv03">Atividade 3</Link>
                         </li>
+                        <li>
+                            <Link to="Atv04">Atividade 4</Link>
+                        </li>
                     </ul>
                 </nav>
         </div>
