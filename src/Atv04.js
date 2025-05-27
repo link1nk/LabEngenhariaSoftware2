bc0203088b97425f9a95a3e288617f0e8e9433ff
Lincoln Dias <lincolnroberto820@gmail.com>
2025-05-27 10:11:37 -0300

    subindo atividade 4

---
+++ src/Atv04.js
@@ -0,0 +1,35 @@
+import { Link } from "react-router-dom";
+import Toolbar from "./Toolbar";
+import Gallery from "./Gallery1";
+import Counter from "./Counter";
+import MovingDot from "./MovingDot";
+import Form from "./Form";
+import Form2 from "./Form2";
+import Form3 from "./Form3";
+import List1 from "./List1";
+import List2 from "./List2";
+import ShapeEditor from "./ShapeEditor";
+import CounterList from "./CounterList";
+
+function Atividade04() {
+    return(
+
+         <>
+            <h1>Atividade 04</h1><br/><br/>
+            <Toolbar /><br/><br/>
+            <Gallery /><br/><br/>
+            <Counter /><br/><br/>
+            <MovingDot /><br/><br/>
+            <Form /><br/><br/>
+            <Form2 /><br/><br/>
+            <Form3 /><br/><br/>
+            <List1 /><br/><br/>
+            <List2 /><br/><br/>
+            <ShapeEditor /><br/><br/>
+            <CounterList /><br/><br/>
+        </> 
+
+    );
+}
+
+export default Atividade04;
--- src/minhasRotas.js
+++ src/minhasRotas.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Home";
 import Atv01 from "./Atv01";
 import Atv02 from "./Atv02";
-import Atv03 from "./Atv03";
+import Atv04 from "./Atv04";
 
 export default function minhasRotas(){
     return(
@@ -13,6 +13,7 @@ export default function minhasRotas(){
                 <Route path="/atv01" element={<Atv01 />} />
                 <Route path="/atv02" element={<Atv02 />} />
                 <Route path="/atv03" element={<Atv03 />}/>
+                <Route path="/atv04" element={<Atv04 />}/>
             </Routes>
         </BrowserRouter>
 
